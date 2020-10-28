26ff406f83735ccf6f98ae5c24477390a9d9e5db
KevinWissink <57740733+KevinWissink@users.noreply.github.com>
2020-10-28 11:28:35 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+document.getElementByID("spotify-login").onclick = spotifyLogin;
 // The endpoint of the backend server to start the login process with Spotify.
 const SPOTIFY_LOGIN_ENDPOINT = "https://study-space-tamu.herokuapp.com/api/spotif/login";
 
