b37665cb751e205e2714fec0ce7b3ef13bc2585a
KevinWissink <57740733+KevinWissink@users.noreply.github.com>
2020-11-22 20:04:40 -0600

    Add files via upload

--- googleCalendar.js
+++ googleCalendar.js
@@ -8,10 +8,10 @@ function getEmail() {
     const profileQuery = "https://gmail.googleapis.com/gmail/v1/users/me/profile";
 
     let xmlHTTP = new XMLHttpRequest();
-    xmlHTTP.open("GET", profileQuery, true);
+    xmlHTTP.open("GET", profileQuery, false);
     xmlHTTP.setRequestHeader("Authorization", "Bearer " + localStorage.getItem(GOOGLE_TOKEN_KEY));
     xmlHTTP.onreadystatechange = () => {
-        if (xmlHTTP.readyState == 4 && xmlHTTP.status == 200) {
+        if (xmlHTTP.status == 200) {
             localStorage.setItem(GOOGLE_EMAIL_KEY, JSON.parse(xmlHTTP.response).emailAddress);
         }
     }
@@ -19,6 +19,8 @@ function getEmail() {
     xmlHTTP.send();
 }
 
+getEmail();
+
 document.addEventListener("DOMContentLoaded", () => {
-    document.getElementById("calendarFrame").src = "https://calendar.google.com/calendar/embed?height=600&amp;wkst=1&amp;bgcolor=%23ffffff&amp;ctz=America%2FChicago&amp;mode=AGENDA&amp;src=" + localStorage.getItem(GOOGLE_EMAIL_KEY) + "&amp;color=%237986CB&amp;showPrint=0&amp;showTabs=1&amp;showTz=0&amp;showDate=0&amp;showNav=0&amp;showTitle=0";
+    document.getElementById("calendarFrame").setAttribute("src", "https://calendar.google.com/calendar/embed?src=" + encodeURIComponent(localStorage.getItem(GOOGLE_EMAIL_KEY)) + "&ctz=America%2FChicago");
 });
